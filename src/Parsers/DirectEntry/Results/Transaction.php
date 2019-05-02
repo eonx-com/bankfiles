55893a8fff16754d49ee7c69955a815d07b1b246
rashmitsingh <47509325+rashmitsingh@users.noreply.github.com>
2019-05-03 09:28:23 +1000

    PYMT-636:DE files processor (#14)
    * DE files processor
    
    * Code cleanup
    
    * Removed usages of trim
    
    * Merge development

---
+++ src/Parsers/DirectEntry/Results/Transaction.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+namespace EoneoPay\BankFiles\Parsers\DirectEntry\Results;
+
+use EoneoPay\BankFiles\Parsers\BaseResult;
+use EoneoPay\BankFiles\Parsers\DirectEntry\HasAmounts;
+
+/**
+ * @method string|null getAccountName()
+ * @method string|null getAccountNumber()
+ * @method string|null getBsb()
+ * @method string|null getIndicator()
+ * @method string|null getLodgmentReference()
+ * @method string|null getRecordType()
+ * @method string|null getRemitterName()
+ * @method string|null getTraceAccountNumber()
+ * @method string|null getTraceBsb()
+ * @method string|null getTxnCode()
+ */
+class Transaction extends BaseResult
+{
+    use HasAmounts;
+
+    /**
+     * Get amount on transaction as dollar
+     *
+     * @return string
+     */
+    public function getAmount(): string
+    {
+        return $this->createAmount($this->data['amount'] ?? '');
+    }
+
+    /**
+     * Get withholding tax on transaction as dollar
+     *
+     * @return string
+     */
+    public function getWithholdingTax(): string
+    {
+        return $this->createAmount($this->data['withholdingTax'] ?? '');
+    }
+
+    /**
+     * Return object attributes.
+     *
+     * @return string[]
+     */
+    protected function initAttributes(): array
+    {
+        return [
+            'accountName',
+            'accountNumber',
+            'amount',
+            'bsb',
+            'indicator',
+            'lodgmentReference',
+            'recordType',
+            'remitterName',
+            'traceAccountNumber',
+            'traceBsb',
+            'txnCode',
+            'withholdingTax'
+        ];
+    }
+}
